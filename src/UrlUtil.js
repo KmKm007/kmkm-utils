98947a78fee6a36680c9fa1b368ac0b28895af77
KmKm <kmkmhuang007@gmail.com>
2017-08-08 11:30:37 +0800

    update UrlUtil

--- src/UrlUtil.js
+++ src/UrlUtil.js
@@ -3,13 +3,10 @@
         typeof define === 'function' && define.amd ? define(factory) : (global.UrlUtil = factory())
 })(this, function() {
     const UrlUtil = {}
-    UrlUtil.getQueryString = function(name) {
-        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
-        var r = window.location.search.substr(1).match(reg)
-        if (r != null) {
-            return unescape(r[2])
-        }
-        return null
+    UrlUtil.getQueryString = function(key) {
+      var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
+      var result = window.location.search.substr(1).match(reg);
+      return result?decodeURIComponent(result[2]) : null;
     }
 
     return UrlUtil
